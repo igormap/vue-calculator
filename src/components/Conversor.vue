9d30aa266ff9c9f4a38b773d0f671c6c80df3b23
Igor Martins <igormartins16@gmail.com>
2021-07-05 18:17:06 -0400

    add functions

--- src/components/Conversor.vue
+++ src/components/Conversor.vue
@@ -2,7 +2,7 @@
   <div class="conversor">
     <h2>{{moedaA}} Para {{moedaB}}</h2>
     <input type="text" v-model="moedaA_value" v-bind:placeholder="moedaA">
-    <input type="button" value="Converter">
+    <input type="button" value="Converter" v-on:click="converter">
     <h2>{{moedaB_value}}</h2>
   </div>
 </template>
@@ -13,8 +13,25 @@
     props: ["moedaA", "moedaB"],
       data(){
         return{
-          moedaA_value : '',
-          moedaB_value : 0
+          moedaA_value: "",
+          moedaB_value: 0
+        };
+      },
+      methods: {
+        converter() {
+          let de_para = this.moedaA + "_" + this.moedaB
+
+          let url = "https://free.currconv.com/api/v7/convert?q=" 
+            de_para
+          + "&compact=ultra&apiKey=7a108539468fb667623a"
+
+          fetch(url)
+            .then(res => {
+              return res.json()})
+            .then(json => {
+              let cotacao = json[de_para].val
+              this.moedaB_value = (cotacao * parseFloat(this.moedaA_value)).toFixed(2)
+            })
         }
       }
   }
